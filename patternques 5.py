9aa2808a61629a1b2731f30e154e5b0286aaa57b
Amit lavania <amit.lavania_cs20@gla.ac.in>
2021-03-18 09:48:19 +0530

    Delete patternques 5.py

--- patternques 5.py
+++
@@ -1,9 +0,0 @@
-#pattern question 
-n=eval(input("enter the number of line"))
-#ch=65
-for i in range(1,n+1):
-
-	#for j in range(1,i+1):
-	#	print(chr(ch),end=" ")
-	#	ch+=1
-	print(' '*(n-i)+'*'*(2*i-1))
